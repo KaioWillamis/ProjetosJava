efdb9e7571bfbf5ad15f87f581aa32f4239a9bcc
KaioWillamis <brm560548@gmail.com>
2025-02-26 14:01:11 -0300

    atualzei

--- EstruturaDecisão/src/ResultadoEscolar.java
+++ EstruturaDecisão/src/ResultadoEscolar.java
@@ -4,6 +4,6 @@ public class ResultadoEscolar {
 
         String resultado = nota >= 7 ? "Aprovado" : nota >=5 && nota < 7 ? "Recuperação" : "Reprovado" ;
 
-        System.out.println(resultado);
+        System.out.println(resultado);;
     }
 }
