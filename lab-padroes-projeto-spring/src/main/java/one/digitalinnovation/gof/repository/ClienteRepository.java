825cbe04b2e3311786e5623a42f8c2dbcde464bf
KaioWillamis <brm560548@gmail.com>
2025-05-13 18:08:42 -0300

    atualizeei

---
+++ lab-padroes-projeto-spring/src/main/java/one/digitalinnovation/gof/repository/ClienteRepository.java
@@ -0,0 +1,8 @@
+package one.digitalinnovation.gof.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClienteRepository extends CrudRepository<Cliente,Long>{
+    
+}sdfgdfgfgrrt
